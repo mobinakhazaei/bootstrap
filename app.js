d383950f018538bc5d2611224373a2e02db164e7
helia <heliahashmimoghaddam@gmail.com>
2024-01-27 21:36:45 +0330

    Know more

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ window.alert("Thank you for choosing this site");
 function walterWhite(){
     location.replace("https://www.crunchbase.com/person/walter-white-1f80")
 }
+function sarahJhonson (){
+    location.replace("https://www.linkedin.com/in/sarah-johnson-8789a381")
+}
+function williamAnderson(){
+    location.replace("https://www.crunchbase.com/person/william-anderson-9b13")
+}
+function amandaJepson(){
+    location.replace("http://scholarsnetwork.org/about-us")
+}
